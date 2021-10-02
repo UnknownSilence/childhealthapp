9c7de855a1767d7f80698fec25eff5d751f1e6f8
Trent <TrentThomasHardy@yahoo.com>
2021-10-02 10:51:42 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,9 +3,19 @@ import { Grid, Typography } from "@material-ui/core";
 
 const About = () => {
   return (
-    <Grid container>
+    <Grid>
       <Typography variant="h1" component="h2">
         About
+      </Typography>
+      <Typography>&nbsp;</Typography>
+      <Typography variant="body3">
+        Nowadays, when children are diagnosed with an ailment or illness, they
+        are given a great number of documents to read through describing their
+        ailment and what medication they must take to get treated. However, many
+        children dislike the idea of reading through all those documents and
+        would most likely disregard them. This web application game was designed
+        to better education children on their ailment and treatment by gamifying
+        the learning process and making it much more enjoyable.
       </Typography>
     </Grid>
   );
