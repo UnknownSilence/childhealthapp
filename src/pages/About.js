3d3fa3ef82000843c33dc63076640977c6f9707e
Trent <TrentThomasHardy@yahoo.com>
2021-10-02 10:56:03 -0500

    ui update

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,6 @@ const About = () => {
       </Typography>
       <Typography>&nbsp;</Typography>
       <Typography variant="body3">
-<<<<<<< HEAD
         Nowadays, when children are diagnosed with an ailment or illness, they
         are given a great number of documents to read through describing their
         ailment and what medication they must take to get treated. However, many
@@ -17,14 +16,12 @@ const About = () => {
         would most likely disregard them. This web application game was designed
         to better education children on their ailment and treatment by gamifying
         the learning process and making it much more enjoyable.
-=======
         Nowadays, when children are diagnosed with an ailment or illness, they are given
         a great number of documents to read through describing their ailment and what medication
         they must take to get treated. However, many children dislike the idea of reading through
         all those documents and would most likely disregard them. This web application game was
         designed to better education children on their ailment and treatment by gamifying the 
         learning process and making it much more enjoyable. 
->>>>>>> 73d41cc56ecedff301b73a315b606633c20d3ca1
       </Typography>
     </Grid>
   );
