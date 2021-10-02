93bfc28f5956dcbf08f9faae196634757874150e
Trent <TrentThomasHardy@yahoo.com>
2021-10-02 14:04:22 -0500

    update?

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,15 @@
 import React from "react";
-import { Grid, Typography, Box } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  Box,
+  Item,
+  Divider,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+
+import { ReactComponent as DocOffice } from "./docOffice.svg";
 
 const Game = () => {
   return (
@@ -7,9 +17,39 @@ const Game = () => {
       <Typography variant="h1" component="h2">
         Game
       </Typography>
-      <Typography>&nbsp;</Typography>
-      <Typography variant="body3">I'm a game!</Typography>
-      <Box m={15} pt={10}></Box>
+      <Box m={1} pt={1}>
+        <DocOffice></DocOffice>
+      </Box>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 20 }} gutterBottom>
+            <Typography variant="h6">
+              {" "}
+              There are multiple types of ear infections. One of these
+            </Typography>
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 20 }} gutterBottom>
+            <Typography variant="h6">
+              {" "}
+              There are multiple types of ear infections. One of these
+            </Typography>
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 20 }} gutterBottom>
+            <Typography variant="h6">
+              {" "}
+              There are multiple types of ear infections. One of these
+            </Typography>
+          </Typography>
+        </CardContent>
+      </Card>
     </Grid>
   );
 };
