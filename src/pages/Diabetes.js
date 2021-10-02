f1f4ff05e347300e4d0a91a111f96e120099f34a
Trent <TrentThomasHardy@yahoo.com>
2021-10-02 10:40:21 -0500

    diabetes pae update
    added new ui on diabetes page

--- src/pages/Diabetes.js
+++ src/pages/Diabetes.js
@@ -1,16 +1,109 @@
-import { Typography, Grid } from "@material-ui/core"
-import React from "react"
-import Emoji from "../components/Emoji"
+import {
+  Typography,
+  Grid,
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  Link,
+} from "@material-ui/core";
+import React from "react";
+import Emoji from "../components/Emoji";
+import Image from "material-ui-image";
+import { ReactComponent as DiabPerson } from "./Diabetesperson.svg";
+import { ReactComponent as SugarPic } from "./sugar.svg";
 
-export default function Diabetes(){
-    return(
-        <Grid>
-            <Typography variant="h3"> Diabetes page</Typography>
-            <Typography variant="body1"> Before learning about diabetes, we need to know what glucose is. Glucose is a sugar<Emoji symbol="🧂"/> that the 
-            body gets from food <Emoji symbol="🍞"/> and uses for energy<Emoji symbol="🔋"/>.There are two types of diabetes. In type 1 diabetes, the body stops making insulin or does
-             not make enough. Insulin is a hormone, or chemical, that normally controls the level of glucose in the blood<Emoji symbol="🩸"/>. This type 
-             of diabetes often happens in children<Emoji symbol="🧒"/> and teenagers. In type II the body makes insulin but cannot use it properly. Type II
-              usually occurs in adults<Emoji symbol="👨"/>. It is the most common type of diabetes. In both cases, insulin would be the treatment</Typography>
-        </Grid>
-    )
+export default function Diabetes() {
+  return (
+    <Grid>
+      <Typography variant="h3"> Diabetes</Typography>
+      <Grid item container direction="column" display="flex" justify="center">
+        <Typography>&nbsp;</Typography>
+        <Typography>&nbsp;</Typography>
+        <Typography>&nbsp;</Typography>
+      </Grid>
+      <Grid item container display="flex" justify="center">
+        <DiabPerson></DiabPerson>
+      </Grid>
+      <Grid item container direction="column" display="flex" justify="center">
+        <Typography>&nbsp;</Typography>
+        <Typography>&nbsp;</Typography>
+        <Typography>&nbsp;</Typography>
+      </Grid>
+
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 20 }} gutterBottom>
+            <Typography variant="h6">
+              {" "}
+              Before learning about diabetes, we need to know what glucose is.
+              Glucose is a sugar
+              <Emoji symbol="🧂" /> that the body gets from food{" "}
+              <Emoji symbol="🍞" /> and uses for energy
+              <Emoji symbol="🔋" />.
+            </Typography>
+            <Typography variant="h6">&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+          </Typography>
+          <Typography variant="h6" component="div">
+            There are two types of diabetes. In type 1 diabetes, the body stops
+            making insulin or does not make enough. Insulin is a hormone, or
+            chemical, that normally controls the level of glucose in the blood{" "}
+            <Emoji symbol="🩸" />.
+          </Typography>
+          <Typography variant="h6">&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+          <Typography variant="h6" sx={{ mb: 1.5 }}>
+            This type of diabetes often happens in children
+            <Emoji symbol="🧒" /> and teenagers. In type II the body makes
+            insulin but cannot use it properly. Type II usually occurs in adults
+            <Emoji symbol="👨" />. It is the most common type of diabetes. In
+            both cases, insulin would be the treatment
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <a target="_blank" href="https://kidshealth.org/en/kids/type1.html">
+            <Button size="small">Learn More</Button>
+          </a>
+        </CardActions>
+      </Card>
+      <Grid item container direction="column" display="flex" justify="center">
+        <Typography>&nbsp;</Typography>
+        <Typography>&nbsp;</Typography>
+        <Typography>&nbsp;</Typography>
+      </Grid>
+      <Grid item container display="flex" justify="center">
+        <SugarPic></SugarPic>
+      </Grid>
+      <Grid item container direction="column" display="flex" justify="center">
+        <Typography>&nbsp;</Typography>
+        <Typography>&nbsp;</Typography>
+        <Typography>&nbsp;</Typography>
+      </Grid>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 20 }} gutterBottom>
+            <Typography variant="h6">
+              {" "}
+              To prevent diabetes, it's best to consume a balanced nutritional
+              diet and exercise reguarly!
+            </Typography>
+            <Typography variant="h6">&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+          </Typography>
+          <Typography variant="h6" component="div">
+            Doctors say exercising for atleast 30 minutes a day, five days a
+            week is the day best way to ensure your body is healthy andd capable
+            of using insulin <Emoji symbol="🏋️" />!
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <a
+            target="_blank"
+            href="https://www.johnmuirhealth.com/health-education/conditions-treatments/diabetes-articles/preventing-diabetes.html"
+          >
+            <Button size="small">Learn More</Button>
+          </a>
+        </CardActions>
+      </Card>
+      <Typography variant="body1"></Typography>
+    </Grid>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,30 +2,27 @@ import React from "react";
 import { Grid, Typography, Button, Item, TextField } from "@material-ui/core";
 import { ReactComponent as Sleeping } from "./SleepPatient.svg";
 //import { Link, BrowserRouter as Router } from "react-router-dom";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import { ailments } from "./ailmennts";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-
 const Home = () => {
   const [disease, setdisease] = useState("");
   const history = useHistory();
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if(disease === "Influenza" ){
+    e.preventDefault();
+    if (disease === "Influenza") {
       history.push("/flu");
+    } else if (disease === "Pink Eye") {
+      history.push("/pinkeye");
+    } else if (disease === "Diabetes") {
+      history.push("/diabetes");
+    } else if (disease === "Ear Infection") {
+      history.push("/earinfection");
     }
-    else if(disease === "Pink Eye"){
-      history.push("/pinkeye")
-    }
-    else if(disease === "Diabetes"){
-      history.push("/diabetes")
-    }
-    else if(disease === "Ear Infection"){
-      history.push("/earinfection")
-    }
-  }
+  };
   return (
     <div>
       <Grid container direction="column" item xs={12} align="center">
@@ -49,7 +46,9 @@ const Home = () => {
           alignItems="flex-end"
           justify="center"
         >
-          <Typography>My doctor said I have...</Typography>
+          <Typography>
+            <strong id="promptText">My doctor said I have...</strong>
+          </Typography>
         </Grid>
         <Grid
           item
@@ -74,7 +73,26 @@ const Home = () => {
               />
             )}
           />
-          <Button variant="outline" onClick={handleSubmit}>Submit</Button>
+        </Grid>
+        <Grid item container direction="column" display="flex" justify="center">
+          <Typography>&nbsp;</Typography>
+          <Typography>&nbsp;</Typography>
+        </Grid>
+        <Grid item container direction="column" display="flex" justify="center">
+          <Button
+            style={{
+              borderRadius: 35,
+              backgroundColor: "turquoise",
+              color: "white",
+              padding: "18px 36px",
+              fontSize: "18px",
+            }}
+            variant="contained"
+            onClick={handleSubmit}
+            endIcon={<MenuBookIcon />}
+          >
+            What is that?
+          </Button>
         </Grid>
       </Grid>
     </div>
